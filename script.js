bd542ce22ba86f4a7aad5ca28df8e321dded62db
Regul8r <120161421+Regul8r@users.noreply.github.com>
2023-08-02 16:33:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeColor(el) 
+{
+    document.body.style.backgroundColor = el.value;
+    var colorSelected = document. querySelector ("#theColors").value;   
+    console.log(userInput + " has chosen " + colorSelected);
+    
+  }
